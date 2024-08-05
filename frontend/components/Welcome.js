0eadb8a3488706c425cdac64f4161cc79a18bf71
Sophie Silvestre <silvestre.sophie@gmail.com>
2024-08-05 12:27:08 +0200

    tutorialImprovingSophie

--- frontend/components/Welcome.js
+++ frontend/components/Welcome.js
@@ -4,56 +4,86 @@ import { useState } from 'react'
 function Welcome() {
   const [tutoPage, setTutoPage] = useState(0)
 
-  let style = {}
+  const handleNext = () => {
+    if (tutoPage < 3) {
+      setTutoPage(tutoPage + 1)
+    }
+  }
 
-  let tutoPage1 =
-    <div className={styles.tutoContainer}>
-      <div className={styles.tutoTextContainer}>
-        <h3 className={styles.tutoTitle}>Bienvenue chez Pulsify !</h3>
-        <p className={styles.tutoText}>Votre allié Suno ultime pour générer les invites (prompt) dont vous rêvez !</p>
+  const handlePrev = () => {
+    if (tutoPage > 0) {
+      setTutoPage(tutoPage - 1)
+    }
+  }
 
+  let tutoContent;
+
+  if (tutoPage === 0) {
+    let style = {}
+    tutoContent = (
+      <div className={styles.tutoContainer}>
+        <div className={styles.tutoTextContainer}>
+          <h3 className={styles.tutoTitle}>Bienvenue chez Pulsify !</h3>
+          <p className={styles.tutoText}>Votre allié Suno ultime pour générer les invites (prompt) dont vous rêvez !</p>
+
+        </div>
+        <footer className={styles.tutoFooter}>
+
+          <div className={styles.tutoCarousel}>Tuto Page 1 of 4</div>
+          <button className={styles.btn} onClick={handleNext}>Suivant</button>
+        </footer>
       </div>
-      <footer className={styles.tutoFooter}>
-        <button className={styles.btn}>Précédent</button>
-        <div className={styles.tutoCarousel}>Tuto Page</div>
-        <button className={styles.btn}>Suivant</button>
-      </footer>
-    </div>
+    );
+  } else if (tutoPage === 1) {
+    tutoContent =
+      <div className={styles.tutoContainer}>
+        <div className={styles.tutoTextContainer}>
 
-  let tutoPage2 =
-    <div className={styles.tutoContainer}>
-      <p className={styles.tutoText}>Suno est une startup spécialisée dans la création de musique assistée par intelligence artificielle. Leur technologie permet aux utilisateurs de générer des morceaux de musique à partir de simples descriptions textuelles. La génération de ces morceaux de musique coûtent des crédits.</p>
-      <footer className={styles.tutoFooter}>
-        <button className={styles.btn}>Précédent</button>
-        <div className={styles.tutoCarousel}>Tuto Page</div>
-        <button className={styles.btn}>Suivant</button>
-      </footer>
-    </div>
+          <p className={styles.tutoText}>Suno est une startup spécialisée dans la création de musique assistée par intelligence artificielle. Leur technologie permet aux utilisateurs de générer des morceaux de musique à partir de simples descriptions textuelles. La génération de ces morceaux de musique coûtent des crédits.</p>
+        </div>
+        <footer className={styles.tutoFooter}>
+          <button className={styles.btn} onClick={handlePrev}>Précédent</button>
+          <div className={styles.tutoCarousel}>Tuto Page 2 of 4</div>
+          <button className={styles.btn} onClick={handleNext}>Suivant</button>
+        </footer>
+      </div>
+  } else if (tutoPage === 2) {
+    tutoContent = (
+      <div className={styles.tutoContainer}>
+        <div className={styles.tutoTextContainer}>
+          <h3 className={styles.tutoTitleText}>Economisez vos crédits grâce à des invites de plus en plus pertinentes !</h3>
+          <p className={styles.tutoText}>Choisissez des styles musicaux qui correspondent à ce que vous souhaitez générer et ajustez au fur et à mesure</p>
+          <div >Sans Pulsify, 200 essais. Avec Pulsify, 20 essais</div>
+        </div>
+        <footer className={styles.tutoFooter}>
+          <button className={styles.btn} onClick={handlePrev}>Précédent</button>
+          <div className={styles.tutoCarousel}>Tuto Page 3 of 4</div>
+          <button className={styles.btn} onClick={handleNext}>Suivant</button>
+        </footer>
+      </div>
+    )
+  } else if (tutoPage === 3) {
+    tutoContent = (
+      <div className={styles.tutoContainer}>
+        <div className={styles.tutoTextContainer}>
+          <h3 className={styles.tutoText}>Rappelez-vous des sons générés précédement, et ajustez la note si votre avis a changé</h3>
+          <p className={styles.tutoText}>Mémorisez les styles musicaux et trouvez les mots-clés qui vont bien ensemble</p>
+
+        </div>
+        <footer className={styles.tutoFooter}>
+          <button className={styles.btn} onClick={handlePrev}>Précédent</button>
+          <div className={styles.tutoCarousel}>Tuto Page 4 of 4</div>
+
+        </footer>
+      </div>
+
+    )
+  }
 
 
-  let tutoPage3 =
-    <div className={styles.tutoContainer}>
-      <h3 className={styles.tutoTitleText}>Economisez vos crédits grâce à des invites de plus en plus pertinentes !</h3>
-      <p className={styles.tutoText}>Choisissez des styles musicaux qui correspondent à ce que vous souhaitez générer et ajustez au fur et à mesure</p>
-      <div>Sans Pulsify, 200 essais. Avec Pulsify, 20 essais</div>
-      <footer className={styles.tutoFooter}>
-        <button className={styles.btn}>Précédent</button>
-        <div className={styles.tutoCarousel}>Tuto Page</div>
-        <button className={styles.btn}>Suivant</button>
-      </footer>
-    </div>
 
 
-  let tutoPage4 =
-    <div className={styles.tutoContainer}>
-      <h3 className={styles.tutoText}>Rappelez-vous des sons générés précédement, et ajustez la note si votre avis a changé</h3>
-      <p>Mémorisez les styles musicaux et trouvez les mots-clés qui vont bien ensemble</p>
-      <footer className={styles.tutoFooter}>
-        <button className={styles.btn}>Précédent</button>
-        <div className={styles.tutoCarousel}>Tuto Page</div>
-        <button className={styles.btn}>Suivant</button>
-      </footer>
-    </div>
+
 
 
 
@@ -69,7 +99,7 @@ function Welcome() {
           </div>
         </header>
         <div>
-          {tutoPage1}
+          {tutoContent}
         </div>
       </main>
     </div>

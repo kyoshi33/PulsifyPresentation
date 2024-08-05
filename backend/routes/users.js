4fa9becc45a3f8bd44cf57cc57dee4eee81ca3d1
Morillon <julien.morillon@yahoo.fr>
2024-08-05 10:08:48 +0200

    modules check

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+/* Route se connecter /signin */
+
+router.post('/signin')
+
+
+
 
 module.exports = router;

92232fe95944062c5b4a6fd7cd7c249d1c5157a4
Morillon <julien.morillon@yahoo.fr>
2024-08-05 17:33:45 +0200

    profil

--- frontend/pages/profil.js
+++ frontend/pages/profil.js
@@ -1,12 +1,39 @@
-import '../styles/Profil.module.css';
+import styles from '../styles/Profil.module.css';
+import { login } from '../reducers/user';
 
 
 function Profil() {
-  return (
-    <>
 
-      Test
-    </>
+
+
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.onglet}>
+
+        <div className={styles.bibliotheque}>
+          Ma bibliotheque
+        </div>
+        <div className={styles.favoris}>
+          Favoris
+
+        </div>
+
+
+      </div>
+
+
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+
+
+        </div>
+
+      </div>
+
+
+      <button className={styles.btnRetour} onClick={() => window.location.href = '/'}>Retour</button>
+    </div >
   );
 }
 

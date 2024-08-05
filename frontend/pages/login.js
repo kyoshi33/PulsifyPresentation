c3821403e24d3d45acb3728e7dccb9f4dae00646
Pierre-Damien <pierredamien33500@hotmail.fr>
2024-08-05 11:40:29 +0200

    page connexion

---
+++ frontend/pages/login.js
@@ -0,0 +1,45 @@
+import styles from '../styles/Login.module.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { GoogleLogin } from '@react-oauth/google'
+import { jwtDecode } from "jwt-decode";
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userGoogleEmail, setUserGoogleEmail] = useState('')
+  const [isConnected, setIsConnected] = useState(false)
+
+  let googleBtn = <GoogleLogin
+    onSuccess={(credentialResponse) => {
+      let email = jwtDecode(credentialResponse.credential).email
+      setUserGoogleEmail(email)
+      setIsConnected(true)
+    }}
+    onError={() => {
+      console.log('Login Failed');
+    }}
+  />
+
+  const connexion = () => {
+
+  }
+
+  return (
+    <GoogleOAuthProvider clientId={process.env.ID_GOOGLE}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Connexion</h1>
+        {googleBtn}
+        <h2>ou ...</h2>
+        <input type='text' placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email} className={styles.inputEmail} />
+        <input type='password' placeholder='Mot de passe' onChange={(e) => setPassword(e.target.value)} value={password} className={styles.inputPassword} />
+        <h4>Mot de passe oublié ?</h4>
+        <button className={styles.btn} onClick={() => connexion()}>Connexion</button>
+        <button className={styles.btnRetour}>Retour</button>
+      </div>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default Login;

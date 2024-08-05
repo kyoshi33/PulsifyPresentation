1e84a4845ef7f48e9f0220fb1424e950682ef32e
Heymow <lynckit@hotmail.com>
2024-08-05 10:55:55 +0200

    vérification du format d'email valide

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,11 +7,19 @@ const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 const { checkBody } = require('../modules/tools')
 
-/* POST créer un nouvel utilisateur. */
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+// POST créer un nouvel utilisateur.
 router.post('/signup', (req, res) => {
+  //Vérifier que les champs sont tous fournis
   if (!checkBody(req.body, ['firstname', 'username', 'password', 'email'])) {
     res.json({ result: false, error: 'Champs manquants ou vides' });
     return;
+  }
+  //Vérifier que l'e-mail a un format valide
+  if (!EMAIL_REGEX.test(req.body.email)) {
+    res.json({ result: false, error: 'e-mail invalide' });
   }
 
   // Vérifier que l'utilisateur n'existe pas déjà en base de données
@@ -19,6 +27,7 @@ router.post('/signup', (req, res) => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
+      // Créer le nouvel utilisateur
       const newUser = new User({
         firstname: req.body.firstname,
         username: req.body.username,
@@ -26,17 +35,17 @@ router.post('/signup', (req, res) => {
         password: hash,
         token: uid2(32)
       });
-
       newUser.save().then(newDoc => {
 
         res.json({ result: true, token: newDoc.token, firstname: newDoc.firstname, username: req.body.username, email: req.body.email });
       });
     } else {
-      // L'utilisateur existe déjo en base de données
+      // L'utilisateur existe déjà en base de données
       res.json({ result: false, error: 'Utilisateur déjà existant' });
     }
   });
 });
+
 
 
 

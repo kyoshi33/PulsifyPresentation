93153c945217634db7e4d5b8b6ef58709d1a5f62
Heymow <lynckit@hotmail.com>
2024-08-06 10:11:52 +0200

    google_id : unique + maj css accueil

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
-    google_id: { type: Number, required: true, default: null },
+    google_id: { type: Number, required: true, default: null, unique: true },
     password: { type: String, required: true, default: null },
     token: { type: String, required: true },
     prompts: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'prompts' }], required: false, default: [] },

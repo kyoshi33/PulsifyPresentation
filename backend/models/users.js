8263f023af74a5ff49afc110072064179fc9e474
Heymow <lynckit@hotmail.com>
2024-08-06 09:51:44 +0200

    mise à jour de la collection users pour intégrer l'id Google

--- backend/models/users.js
+++ backend/models/users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, default: null },
+    google_id: { type: Number, required: true, default: null },
     password: { type: String, required: true },
     token: { type: String, required: true },
     prompts: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'prompts' }], required: false, default: [] },

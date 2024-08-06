731c5efe23301dccded6aadba9d6bfcd35ef642b
Heymow <lynckit@hotmail.com>
2024-08-06 11:02:37 +0200

    accueil : css

--- frontend/pages/Accueil.js
+++ frontend/pages/Accueil.js
@@ -50,47 +50,89 @@ function Accueil() {
                             Communauté
                         </div>
                     </div>
-                    <div className={styles.choiceContainer}>
+                    <div className={styles.modelChoiceContainer}>
 
                         <input type='string' placeholder='Recherche...' onChange={(e) => setSearch(e.target.value)} value={search} className={styles.inputSearch} />
 
-                        <button className={styles.listItemContainer}>
-                            <div className={styles.listItemTitle}>
-                                Rockabilly
-                            </div>
-                            <div className={styles.listItemPrompt}>
-                                Jazz, rock, musette, flute
-                            </div>
-                        </button>
+                        <div className={styles.scrollWindow}>
 
-                        <button className={styles.listItemContainer}>
-                            <div className={styles.listItemTitle}>
-                                Rock Indie
-                            </div>
-                            <div className={styles.listItemPrompt}>
-                                rock, electric guitar/bass/drums, pop,folk
-                            </div>
-                        </button>
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
 
-                        <button className={styles.listItemContainer}>
-                            <div className={styles.listItemTitle}>
-                                Modern classical
-                            </div>
-                            <div className={styles.listItemPrompt}>
-                                contemporary, mordern classical, XXcentury
-                            </div>
-                        </button>
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rock Indie
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    rock, electric guitar/bass/drums, pop,folk
+                                </div>
+                            </button>
 
-                        <button className={styles.listItemContainer}>
-                            <div className={styles.listItemTitle}>
-                                Rockabilly
-                            </div>
-                            <div className={styles.listItemPrompt}>
-                                Jazz, rock, musette, flute
-                            </div>
-                        </button>
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Modern classical
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    contemporary, mordern classical, XXcentury
+                                </div>
+                            </button>
+
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
 
 
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
+
+
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
+
+
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
+
+
+                            <button className={styles.listItemContainer}>
+                                <div className={styles.listItemTitle}>
+                                    Rockabilly
+                                </div>
+                                <div className={styles.listItemPrompt}>
+                                    Jazz, rock, musette, flute
+                                </div>
+                            </button>
+
+                        </div>
                     </div>
                 </div>
             </div >

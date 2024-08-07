bacee9d0884f5b5f6adafce0cd2067f0c09dc9ad
Heymow <lynckit@hotmail.com>
2024-08-07 10:53:18 +0200

    mise en place de la ModelCard

--- frontend/pages/Accueil.js
+++ frontend/pages/Accueil.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 
 import Header from "../components/Header";
+import ModelCard from "../components/ModelCard";
 
 let connected = false;
 
@@ -61,83 +62,14 @@ function Accueil() {
                         <input type='string' placeholder='Recherche...' onChange={(e) => setSearch(e.target.value)} value={search} className={styles.inputSearch} />
 
                         <div className={styles.scrollWindow}>
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rock Indie
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    rock, electric guitar/bass/drums, pop,folk
-                                </div>
-                            </button>
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Modern classical
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    contemporary, mordern classical, XXcentury
-                                </div>
-                            </button>
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
-
-                            <button className={styles.listItemContainer}>
-                                <div className={styles.listItemTitle}>
-                                    Rockabilly
-                                </div>
-                                <div className={styles.listItemPrompt}>
-                                    Jazz, rock, musette, flute
-                                </div>
-                            </button>
-
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
+                            <ModelCard />
                         </div>
                     </div>
                 </div>

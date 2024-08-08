93484d43e6afd290b9a1b1eca7c798a724c57d24
Heymow <lynckit@hotmail.com>
2024-08-08 13:02:55 +0200

    keywordsSchema

--- backend/models/keywords.js
+++ backend/models/keywords.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const keywordsSchema = mongoose.Schema({
-    userId: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }], required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
     keyword: { type: String, required: true },
     frequency: { type: Number, required: true, default: 0 },
     average_rating: { type: Number, required: false },

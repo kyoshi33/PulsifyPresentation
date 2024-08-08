f52000522bfa87d448bcfcc4fe4957719a500989
Morillon <julien.morillon@yahoo.fr>
2024-08-08 10:50:17 +0200

    models sign mess

--- backend/models/messages.js
+++ backend/models/messages.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const signalementsSchema = mongoose.Schema({
+const messagesSchema = mongoose.Schema({
 
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: false },
   text: String,
   prompts: { type: mongoose.Schema.Types.ObjectId, ref: 'prompts', required: false },
-  message: { type: mongoose.Schema.Types.ObjectId, ref: 'messages', required: false },
   creationDate: Date,
+  nbSignalements: Number,
 
 
 });
 
-const Signalement = mongoose.model('signalements', signalementsSchema);
+const Message = mongoose.model('messages', messagesSchema);
 
-module.exports = Signalement;
+module.exports = Message;

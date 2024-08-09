f4d77d82941671ca650736e4c4811ca69c47d603
Pierre-Damien <pierredamien33500@hotmail.fr>
2024-08-09 13:58:11 +0200

    picture user sans photo

--- frontend/components/UserCard.js
+++ frontend/components/UserCard.js
@@ -1,7 +1,6 @@
 import styles from '../styles/Profil.module.css';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image'
 
@@ -10,11 +9,11 @@ import Image from 'next/image'
 
 function UserCard(props) {
   const user = useSelector((state) => state.user.value)
-
+  const picture = props.picture === null ? <FontAwesomeIcon icon={faUser} className={styles.icon} /> : <img className={styles.profilesPic} src={props.picture} alt='photo de profil' />
 
   let profil =
     <div className={styles.profilesContainer}>
-      <img className={styles.profilesPic} src={props.picture} alt='photo de profil' />
+      {picture}
 
       <div className={styles.namediv}>
         <h3 className={styles.nom}> {props.firstname}</h3>

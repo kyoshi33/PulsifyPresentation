2b56caca8155f5faebe67e902cc5811defed1cf7
Morillon <julien.morillon@yahoo.fr>
2024-08-12 12:51:22 +0200

    fermeture page signalement

--- frontend/components/SignalementModal.js
+++ frontend/components/SignalementModal.js
@@ -54,7 +54,7 @@ function SignalementModal(props) {
         </div>
         <div className={styles.modalBtnContainer}>
           <button className={styles.btn} onClick={props.onRequestClose}>Annuler</button>
-          <button className={styles.btn} onClick={handleValidation}>Valider</button>
+          <button className={styles.btn} onClick={handleValidation(props.onRequestClose)}>Valider</button>
         </div>
       </div>
     </Modal >

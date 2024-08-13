d77b17357ace9feb41330b155f6c58cbee3fb534
Khaled CHEHAB <lynckit@hotmail.com>
2024-08-13 16:11:56 +0200

    modelcard remove button

--- frontend/components/ModelCard.js
+++ frontend/components/ModelCard.js
@@ -2,21 +2,27 @@ import styles from '../styles/ModelCard.module.css';
 import { useState } from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faHeart, faCircleExclamation, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faHeart, faCircleExclamation, faStar, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import UserCard from './UserCard';
 
 function ModelCard(props) {
     const user = useSelector((state) => state.user.value)
 
+    let removeButton = <></>
+    props.isOwnGenre && (removeButton =
+        <div className={styles.trashIcon} onClick={() => props.handleRemoveGenre(props.genre)}>
+            <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+        </div>)
 
     return (
         <button className={styles.listItemContainer}>
-            <div className={styles.author}>
+            <div className={styles.author} onClick={() => props.handleClick(props.genre)}>
                 <UserCard username={props.username} firstname={props.firstname} picture={props.picture} ></UserCard>
             </div>
-            <div className={styles.genre}>{props.genre}</div>
-            <div className={styles.listItemPrompt}>{props.prompt}</div>
+            <div className={styles.genre} onClick={() => props.handleClick(props.genre)}>{props.genre}</div>
+            <div className={styles.listItemPrompt} onClick={() => props.handleClick(props.genre)}>{props.projects}</div>
+            {removeButton}
         </button>
 
     );
